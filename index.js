e235afb788e7a75bdef2eae3c9865b9cab5a64df
John Kozak <jk@thameslighter.net>
2016-04-15 11:33:32 +0100

    simplify harvey filenames

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var dcl = require('./declare.js');
+
+//??? which of these are mandatory/optional? ???
+
+require('jquery');
+require('jquery-ui');
+
+require('./Core');
+require('./Utils');
+require('./Types');
+require('./Fields');
+require('./Popups');
+require('./DisplayBase');
+require('./DisplayFieldset');
+require('./DisplayForm');
+require('./DisplayMenu');
+require('./DisplayTabs');
+require('./IO');
+require('./Nodes');
+require('./Panel');
+
+module.exports = dcl;
